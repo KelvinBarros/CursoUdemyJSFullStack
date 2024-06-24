fc9243850de06f9eaafd91112e328cec446e332e
KelvinBarros <kelvinbarros92@gmail.com>
2024-06-24 16:05:51 -0300

    aulas adicionadas

---
+++ aula6/script.js
@@ -0,0 +1,25 @@
+//REGRAS PARA CRIAR VARIAVEIS
+
+// NÃO CRIAR VARIAVEIS COM PALAVRAS RESERVADAS
+
+// let if = 10; // Errado, palavra reservada (if é uma palavra reservada)
+
+
+// NÃO USAR VARIAVEIS QUE COMEÇAM COM NUMEROS
+
+//let 1soma = 15; // Errado, nome começa com número (10 é um número)
+
+//DAR NOMES COERENTES AS VARIAVEIS
+
+let nome = 'João'; // Bom nome para variável
+
+let numero1 = 10; // Bom nome para variável
+
+let resultado = numero1 + 5; // Bom nome para variável
+
+//NÃO PODE CONTER ESPAÇOS OU CARACTERES ESPECIAIS 
+//USAR camelCase (primeira letra minúscula, as próximas letras maiúsculas)
+let nomeDoAluno = 'João';
+
+//USAR snake_case (todas as letras minúsculas, separadas por underscore)
+//NÃO REDECLARAR VARIAVEIS COM LET
---
+++ aula7/index.js
@@ -0,0 +1,10 @@
+//const não pode ser alterado após ser declarado
+
+const nome = 'Kelvin';
+
+nome = 'João'; 
+// Isso não funcionará, pois const não pode ser alterado
+
+console.log(nome);
+
+//let pode ser alterado após ser declarado
