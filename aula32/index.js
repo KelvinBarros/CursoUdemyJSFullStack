1e7f88c5cae1db106345db3e1ac06dcbb100879f
KelvinBarros <kelvinbarros92@gmail.com>
2024-07-01 17:59:27 -0300

    aula32

---
+++ aula32/index.js
@@ -0,0 +1,35 @@
+//ATRIBUICAO VIA DESESTRUTURACAO
+
+//        indice 0  1  2  3  4  5  6  7  8
+const numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+const [um, dois, ...resto] = numeros; //rest operation
+
+
+
+
+
+console.log(um, dois);
+console.log(resto);
+
+
+
+
+
+
+
+
+
+
+
+/*
+let a = 'A';
+let b = 'B';
+let c = 'C';
+
+const letras = [b, c, a];
+
+[a, b, c] = letras;
+
+
+
+console.log(a, b, c);*/
