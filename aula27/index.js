e99fa5f9718c2ecc78382e365da684ea5fb456d8
KelvinBarros <kelvinbarros92@gmail.com>
2024-07-01 12:09:56 -0300

    novas aulas

---
+++ aula27/index.js
@@ -0,0 +1,21 @@
+const data = new Date();
+
+
+
+
+console.log(data.toString());
+
+console.log("dia:" , data.getDate());
+console.log("mes:" , data.getMonth());
+console.log("ano:" , data.getFullYear());
+console.log("hora:" , data.getHours());
+console.log("dia da semana:" , data.getDay());
+ /* Para utilizar o getDay, considerar domingo como 0;
+0 - domingo 
+ 1 - segundo 
+ 2 - terça 
+ 3 - quarta 
+ 5- quinta 
+ 6 - sexta 
+ 7 - sabado
+*/
