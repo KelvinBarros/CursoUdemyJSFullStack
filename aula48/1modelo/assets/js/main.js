65626f0f74037420be7a1ba5a0b02d0e4aadd33e
KelvinBarros <kelvinbarros92@gmail.com>
2024-07-09 18:04:51 -0300

    lista

--- aula48/1modelo/assets/js/main.js
+++ aula48/1modelo/assets/js/main.js
@@ -1,8 +1,37 @@
-const tarefa = document.querySelector('#tarefa');
+document.addEventListener('DOMContentLoaded', carregarTarefas);
 
-function adicionar(){
+function adicionar() {
+    
+    const inputTarefa = document.querySelector('#inputTarefa');
+    const tarefa = inputTarefa.value;
+
+
+    if (tarefa === '') {
+        alert('Preencha o campo da tarefa');
+        return;
+    }
+
     const item = document.createElement('li');
-    item.textContent = tarefa.value;
-    document.querySelector('#listTarefa').appendChild(item);
-    tarefa.value = '';
+    item.textContent = tarefa;
+    document.querySelector('.listTarefa').appendChild(item);
+    inputTarefa.value = '';
+ 
+
+    // criar botão do lado do li
+    const botaoExcluir = document.createElement('button');
+    botaoExcluir.textContent = 'Excluir';
+    botaoExcluir.addEventListener('click', () => {
+        item.remove();
+    });
+    item.appendChild(botaoExcluir);
+    
 }
+
+// acionar a funcao adicionar, quando pressionar enter
+
+document.querySelector('#inputTarefa').addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+        adicionar();
+    }
+});
+
