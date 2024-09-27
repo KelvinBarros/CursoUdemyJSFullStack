378e6d13c9f9af1c08ae51d1b5a3d6fb8d19cf08
KelvinBarros <124080536+KelvinBarros@users.noreply.github.com>
2024-09-27 17:50:58 -0300

    finalizado calc com função construtora

--- aula57exe/1modelo/assets/js/main.js
+++ aula57exe/1modelo/assets/js/main.js
@@ -1,16 +1,51 @@
 function Calculadora(){
     this.display = document.querySelector('.display');
     
+    this.inicia = () => {
+        this.cliqueBotoes();
+        this.capturaEnter();
+    }
+
+    this.capturaEnter = () => {
+        document.addEventListener('keyup', event => {
+            if (event.key === 'Enter') 
+                this.realizaConta();
+        });
+    }
     this.cliqueBotoes = () => {
         document.addEventListener('click', event => {
             const el = event.target;
-            if (el.classList.contains('btn-num')) this.addNum
-        })
+            if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+            if (el.classList.contains('btn-clear')) this.clear();
+            if (el.classList.contains('btn-del')) this.del();
+            if (el.classList.contains('button-eq')) this.realizaConta();
+
+        });
+    };
+
+    this.realizaConta = () => {
+        try {
+            const resultado = eval(this.display.value);
+            
+            if(!resultado){
+                alert('conta invalida');
+                return;
+            }
+
+            this.display.value = resultado;
+        } catch (error) {
+            this.display.value = 'Conta invalida';
+            return;
+        }
     }
 
-    this.inicia = function() {
-        this.cliqueBotoes();
-    };
+
+    this.addNumDisplay = el => {
+        this.display.value += el.innerText
+        this.display.focus();
+    }; 
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
+    this.clear = () => this.display.value = '';
 }
 
 const calculadora = new Calculadora();
