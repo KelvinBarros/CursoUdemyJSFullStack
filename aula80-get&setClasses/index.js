43379075b7c90017d3a533cf75addf92dc7339c3
KelvinBarros <kelvinbarros92@gmail.com>
2025-02-05 18:59:44 -0300

    Update index.js

--- aula80-get&setClasses/index.js
+++ aula80-get&setClasses/index.js
@@ -1,6 +1,26 @@
 class Carro {
-    constructor(marca, modelo) {
-        this.marca = marca;
-        this.modelo = modelo;
+    constructor(nome, ) {
+        this.nome = nome;
+        this.velocidade = 0;
+    }
+    
+    acelerar() {
+        if(this.velocidade >= 100) return;
+        this.velocidade ++;
+        
+    }
+
+    frear() {
+        if(this.velocidade <= 0) return;
+        this.velocidade --;
     }
 }
+
+const c1 = new Carro('Fusca');
+
+
+for(let i = 0; i <= 200; i++) {
+    c1.acelerar();
+}
+
+console.log(c1);
