182d83b6a82265f624a64c6321417a5e679e15b6
KelvinBarros <124080536+KelvinBarros@users.noreply.github.com>
2025-09-05 13:15:41 -0300

    Add username validation to form and fix typo
    Implemented username validation in the form to ensure it contains only letters and numbers and is between 3 and 12 characters. Also fixed a typo in the HTML instructions.

--- aula84-validandoForm/script.js
+++ aula84-validandoForm/script.js
@@ -1,51 +1,71 @@
-class ValidarFormulario{
-    constructor(){
+class ValidarFormulario {
+    constructor() {
         this.formulario = document.querySelector('.formulario');
         this.eventos();
     }
 
-    eventos(){
+    eventos() {
         this.formulario.addEventListener('submit', e => {
             this.handleSubmit(e);
         });
     }
 
-    handleSubmit(e){
+    handleSubmit(e) {
         e.preventDefault();
         const camposValidos = this.camposSaoValidos();
     }
 
-    camposSaoValidos(){
+    camposSaoValidos() {
         let valid = true
 
-        for(let errorText of this.formulario.querySelectorAll('.error-text')){
+        for (let errorText of this.formulario.querySelectorAll('.error-text')) {
             errorText.remove();
         }
 
-        for(let campo of this.formulario.querySelectorAll('.validar')){
-            const label = campo.previousElementSibling.innerHTML.slice(0,-1);
-            if(!campo.value){
+        for (let campo of this.formulario.querySelectorAll('.validar')) {
+            const label = campo.previousElementSibling.innerHTML.slice(0, -1);
+            if (!campo.value) {
                 this.criaErro(campo, `Campo ${label} está vazio`);
                 valid = false
             }
 
-            if (campo.classList.contains('cpf')){
-                if(!this.validaCPF(campo)) valid = false
+            if (campo.classList.contains('cpf')) {
+                if (!this.validaCPF(campo)) valid = false;
+            }
+
+            if (campo.classList.contains('usuario')) {
+                if (!this.validaUsuario(campo)) valid = false;
             }
         }
 
-
     }
 
-    validaCPF(campo){
+    validaCPF(campo) {
         const cpf = new ValidaCPF(campo.value);
-        if(!cpf.valid()){
+        if (!cpf.valida()) {
             this.criaErro(campo, 'CPF invalido');
             return false;
         }
+        return true
     }
 
-    criaErro(campo,mensagem){
+    validaUsuario(campo) {
+        const usuario = campo.value;
+        let valid = true
+        if (usuario.length > 12 || usuario.length < 3) {
+            this.criaErro(campo, 'Usuário deve ter entre 3 e 12 caracteres');
+            valid = false;
+        }
+
+        if (!usuario.match(/[a-zA-Z0-9]+$/g)) {
+            this.criaErro(campo, 'Usuário deve ter apenas letras e/ou numeros');
+            valid = false;
+        }
+
+        return true
+    }
+
+    criaErro(campo, mensagem) {
         const div = document.createElement('div');
         div.innerHTML = mensagem;
         div.classList.add('error-text');
